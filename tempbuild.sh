bfc5b76d5da8f361755507a97c7acee0f2810a90
Boschert <ak1usr3063@lzlbdvjp1004.giolab.local>
2025-07-30 14:24:23 -0400

    whoops

--- tempbuild.sh
+++
@@ -1,7 +0,0 @@
-OUTPUT_DIR="whisperjni-build"
-docker build -f dockerfile . -t whisperjni_binary:test --load
-docker run \
-  -v "$(pwd)/$OUTPUT_DIR:/app/$OUTPUT_DIR" \
-  -v "$(pwd)/test-results:/app/build/test-results" \
-  -e VULKAN_ARG=OFF \
-  whisperjni_binary:test
