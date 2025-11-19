b59539f6c0c71a032383d713c377fc766cfc1593
Jaffe2718 <qqyttwqeei@163.com>
2025-11-19 11:21:56 +0800

    test cuda on windows

--- build_cuda.ps1
+++ build_cuda.ps1
@@ -3,7 +3,7 @@ $TMP_DIR="tmp-build"
 $TARGET_DIR="whisperjni-build"
 
 New-Item -Path $TARGET_DIR -ItemType Directory -Force
-cmake -B build -DCMAKE_BUILD_TYPE=Release "-DCMAKE_INSTALL_PREFIX=$TMP_DIR" -DGGML_STATIC=1 -DGGML_CUDA=1 -DCMAKE_CXX_FLAGS="-std=c++20"
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=1 "-DCMAKE_INSTALL_PREFIX=$TMP_DIR" -DGGML_BACKEND_DL=1 -DGGML_STATIC=1 -DGGML_CUDA=1 -DCMAKE_CXX_FLAGS="-std=c++20"
 cmake --build build --config Release
 cmake --install build
 
