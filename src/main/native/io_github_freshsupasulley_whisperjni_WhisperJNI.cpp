54e147f9e0e1483eb4fcf129d294c70e8a8c5b70
Jaffe2718 <qqyttwqeei@163.com>
2025-11-23 10:55:35 +0800

    add log for `ggml_backend_load_all()`

--- src/main/native/io_github_freshsupasulley_whisperjni_WhisperJNI.cpp
+++ src/main/native/io_github_freshsupasulley_whisperjni_WhisperJNI.cpp
@@ -174,13 +174,13 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
     JNIEnv *env = NULL;
     jint result = -1;
 
-    if (vm->GetEnv((void **) &env, JNI_VERSION_1_6) != JNI_OK) {
+    if (vm->GetEnv((void **) &env, JNI_VERSION_1_4) != JNI_OK) {
         return result;
     }
 
     ggml_backend_load_all();
-
-    result = JNI_VERSION_1_6;
+    std::cout << "Loaded all ggml backends" << std::endl;
+    result = JNI_VERSION_1_4;
     return result;
 }
 
