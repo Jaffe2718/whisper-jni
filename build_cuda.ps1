b9039361e1ea964cb51601ad809f640c79de8247
Jaffe2718 <qqyttwqeei@163.com>
2025-11-24 13:30:39 +0800

    install debug

--- build_cuda.ps1
+++ build_cuda.ps1
@@ -5,7 +5,7 @@ $TARGET_DIR="whisperjni-build"
 New-Item -Path $TARGET_DIR -ItemType Directory -Force
 cmake -B build -DCMAKE_BUILD_TYPE=Debug "-DCMAKE_INSTALL_PREFIX=$TMP_DIR" -DGGML_CUDA=ON -DGGML_STATIC=ON
 cmake --build build --config Debug -j 4
-cmake --install build
+cmake --install build --config Debug
 
 # Move all DLLs from build dir to prod dir
 Move-Item -Path "$TMP_DIR\*.dll" -Destination $TARGET_DIR -Force
