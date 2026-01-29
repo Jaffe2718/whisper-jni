c9d5e368bb007e64a6bb7e2662ed54b59c90de6c
Jaffe2718 <qqyttwqeei@163.com>
2026-01-29 19:16:00 +0800

    1.0.0: musl 8

--- build_cuda.sh
+++ build_cuda.sh
@@ -16,9 +16,9 @@ build_lib() {
     cmake -B build $CMAKE_ARGS \
         -DCMAKE_C_COMPILER=${CC:-musl-gcc} \
         -DCMAKE_CXX_COMPILER=${CXX:-musl-g++} \
-        -DCMAKE_C_FLAGS=\"${MUSL_CFLAGS}\" \
-        -DCMAKE_CXX_FLAGS=\"${MUSL_CFLAGS}\" \
-        -DCMAKE_SHARED_LINKER_FLAGS=\"${MUSL_LDFLAGS}\" \
+        -DCMAKE_C_FLAGS="${MUSL_CFLAGS}" \
+        -DCMAKE_CXX_FLAGS="${MUSL_CFLAGS}" \
+        -DCMAKE_SHARED_LINKER_FLAGS="${MUSL_LDFLAGS}" \
         -DCMAKE_FIND_ROOT_PATH=/opt/musl/x86_64-linux-musl \
         -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
         -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY \
--- build_linux.sh
+++ build_linux.sh
@@ -14,12 +14,11 @@ build_lib() {
     MUSL_LDFLAGS="-L/opt/musl/${MUSL_ARCH:-x86_64}-linux-musl/lib -lc -lm -static-libgcc -static-libstdc++"
 
     cmake -B build $CMAKE_ARGS \
-        -DCMAKE_C_FLAGS="${CMAKE_CFLAGS}" \
         -DCMAKE_C_COMPILER=${CC:-musl-gcc} \
         -DCMAKE_CXX_COMPILER=${CXX:-musl-g++} \
-        -DCMAKE_C_FLAGS=\"${CMAKE_CFLAGS} ${MUSL_CFLAGS}\" \
-        -DCMAKE_CXX_FLAGS=\"${MUSL_CFLAGS}\" \
-        -DCMAKE_SHARED_LINKER_FLAGS=\"${MUSL_LDFLAGS}\" \
+        -DCMAKE_C_FLAGS="${CMAKE_CFLAGS} ${MUSL_CFLAGS}" \
+        -DCMAKE_CXX_FLAGS="${MUSL_CFLAGS}" \
+        -DCMAKE_SHARED_LINKER_FLAGS="${MUSL_LDFLAGS}" \
         -DCMAKE_FIND_ROOT_PATH=/opt/musl/${MUSL_ARCH}-linux-musl \
         -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
         -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY \
